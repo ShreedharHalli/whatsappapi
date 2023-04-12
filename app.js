5d3820b2a21adec70e8fa754d37b8c61d7689fa7
Shreedhar Halli <mayurbusinesssolutions@gmail.com>
2023-04-12 12:07:28 +0530

    created generateqrcode function

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
@@ -7,19 +7,32 @@ const { DynamoDBClient, ScanCommand } = require('@aws-sdk/client-dynamodb');
 app.use(bodyParser.json())
 app.set('view engine', 'ejs')
 
+// Middleware to handle URL encoded data
+app.use(express.urlencoded({ extended: true }));
+
 
 // DYNAMO DB CONNECTION
 const dynamoDB = new DynamoDBClient({
   region: "ap-south-1",
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    accessKeyId: 'AKIAXRFBOUIF7X4Z3K4D',
+      secretAccessKey: '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'
   }
 });
 
 
+//SHOW BASIC WITH BUTTON TO GENERATE QR CODE
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 
+app.post('/generateqrcode', (req, res) => {
+  console.log('called');
+  const formData = req.body;
+  // Do something with the form data here
+  console.log(formData);
+})
 
 // SHOW ALL CLIENTS
 app.get('/showclients', async function (req, res) {
@@ -50,3 +63,6 @@ const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Port listening on ${PORT}`)
 })
+
+
+
