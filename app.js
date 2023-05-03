c85f30116349e3366227626a38ba26f538c91eda
Shreedhar Halli <mayurbusinesssolutions@gmail.com>
2023-05-04 02:48:22 +0530

    added method messagemedia.fromurl

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.use(bodyParser.json())
 app.set('view engine', 'ejs')
 
 // Middleware to handle URL encoded data
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 const clientsFilePath = path.join(__dirname, 'clients.json');
 
 // DYNAMO DB CONNECTION
@@ -224,17 +224,18 @@ app.post('/sendmessage/:tokenKey', async (req, res) => {
     let obj = sessions.find((item) => item.id === clientid);
   if (obj) {
     let contact = req.body.to_number;
-    let fileType = req.body.type;
-    let file = req.body.message;
-    let filename = req.body.filename;
+    let fileURL = req.body.type;
+    // let file = req.body.message;
+    // let filename = req.body.filename;
     let stringedContact = contact.toString();
     if (stringedContact.length === 10) {
       let client = obj.client;
       let mobNoAsUID = `91${stringedContact}@c.us`;
       // your code here
-     const media = new MessageMedia(fileType, file)
+    //  const media = new MessageMedia(fileType, file)
     // const media = MessageMedia.fromFilePath('./AstralGreen.jpg');
     // const media = MessageMedia.fromFilePath('./123.pdf');
+    const media = await MessageMedia.fromUrl(fileURL);
     console.log(media);
      await client.sendMessage(mobNoAsUID, media).then(response => {
       res.status(200).json({
